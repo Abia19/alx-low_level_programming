3a07414c6402930fde30dd07dc67556182c10b6f
Abia19 <chinelo2345@gmail.com>
2023-02-24 23:31:55 -0800

    TASK 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,7 +17,7 @@ void print_remaining_days(int month, int day, int year)
 		{
 			days++;
 		}
-		printf("Day of the year: %d\n", days);
+		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 365 - day);
 	}
 	else
@@ -29,7 +29,7 @@ void print_remaining_days(int month, int day, int year)
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - days);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }

5221e73fa3904044b5c6afdcd2413bccf8bfb0b5
Abia19 <chinelo2345@gmail.com>
2023-02-27 07:46:01 -0800

    TK 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,11 @@
 void print_array(int *a, int n)
 {
 	int k;
-	
+
 	for (k = 0; k < n; k++)
 	{
-		printf("%d\n", a[k]);
-		if (k != (n - 1))
+		printf("%d", a[k]);
+		if (k != (k - 1))
 		{
 			printf(",");
 		}
